8a395c75da693e64a5394c00bcd738b613aa7bf9
Amrhalawani <eng.amrhalawani@rocketmail.com>
2017-11-26 17:39:23 +0200

    Initial commit

---
+++ app/src/main/java/com/AmrH/oncalllock/AdminPolicyReceiver.java
@@ -0,0 +1,10 @@
+package com.AmrH.oncalllock;
+
+import android.app.admin.DeviceAdminReceiver;
+
+/**
+ * Created by Eslam Elhoseiny on 9/15/2017.
+ */
+
+public class AdminPolicyReceiver extends DeviceAdminReceiver {
+}
